a0193ff81863f453b7414c822b1ed48f6c1589f1
Bautista Valle <b.valle.ventas@gmail.com>
2026-06-10 02:40:17 -0300

    mejoras tema oscuro

--- script.js
+++ script.js
@@ -14,10 +14,45 @@ botontema.addEventListener ("click", function() {
     if (temaoscuro === false) {
         document.body.style.backgroundColor= "#222222";
         document.body.style.color = "#ffffff";
+        document.querySelector(".encabezado").style.backgroundColor = "#ffffff";
+        document.querySelector(".encabezado").style.color= "#222222";
+        document.querySelectorAll(".tarjeta").forEach(function(tarjeta){
+            tarjeta.style.backgroundColor ="#ffffff";
+        });
+        document.querySelectorAll (".tarjeta p").forEach(function(p){
+            p.style.color = "#333333";
+        });
+        document.querySelectorAll("section > p").forEach(function(p){
+            p.style.color ="#ffffff";
+        });
+        document.querySelectorAll("h3").forEach(function(h3){
+            h3.style.color = "#222222";
+        });
+        document.querySelectorAll("h2").forEach(function(titulo){
+            titulo.style.color = "#ffffff";
+        });
+
         temaoscuro= true;
-    } else {
-        document.body.style.backgroundColor = "#f0f0f0";
+    }   else {
+        document.body.style.backgroundColor = "#ffffff";
         document.body.style.color = "#222222";
+        document.querySelector (".encabezado").style.backgroundColor = "#222222";
+        document.querySelector (".encabezado").style.color= "#ffffff";
+        document.querySelectorAll(".tarjeta").forEach(function(tarjeta){
+            tarjeta.style.backgroundColor = "#222222";
+        });
+        document.querySelectorAll (".tarjeta p").forEach(function(p){
+            p.style.color = "#ffffff";
+        });
+        document.querySelectorAll("section > p").forEach(function(p){
+            p.style.color ="#222222";
+        });
+        document.querySelectorAll("h3").forEach(function(h3){
+            h3.style.color = "#ffffff";
+        });
+        document.querySelectorAll("h2").forEach(function(titulo) {
+            titulo.style.color ="#222222";
+        });
         temaoscuro= false;
     }
 });
